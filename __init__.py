ded270715a273c843899ddea557d037d77a5d290
William So <polyipseity@gmail.com>
2022-11-02 16:59:57 +0800

    Configure 'bl_info' in '__init__.py'
    Signed-off-by: William So <polyipseity@gmail.com>

--- __init__.py
+++ __init__.py
@@ -1,7 +1,24 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import types as _types
 import typing as _typing
 
 from . import main
 from .bccelerator.util import utils as _utils
+
+bl_info: _typing.Mapping[str, str | tuple[int, int, int]] = _types.MappingProxyType({
+    'name': 'bccelerator',
+    'description': 'Contains tools to accelerate Blender workflow.',
+    'author': 'William So',
+    'version': (1, 0, 0),
+    'blender': (3, 30, 0),
+    'location': 'Multiple locations',
+    'warning': '',
+    'doc_url': '',  # template: {BLENDER_MANUAL_URL}/addons/(location)
+    'tracker_url': '',
+    'support': 'COMMUNITY',
+    'category': 'General',
+})
 
 register: _typing.Callable[[], None] = main.register
 unregister: _typing.Callable[[], None] = main.unregister
