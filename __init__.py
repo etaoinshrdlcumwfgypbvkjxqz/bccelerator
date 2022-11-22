3590855651fc23a397bf03683116b3297297261a
William So <polyipseity@gmail.com>
2022-11-22 23:10:03 +0800

    Bump version from v1.2.0 to v1.2.1
    Signed-off-by: William So <polyipseity@gmail.com>

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ bl_info = {
     'name': 'bccelerator',
     'description': 'Contains tools to accelerate Blender workflow.',
     'author': 'William So',
-    'version': (1, 2, 0),
+    'version': (1, 2, 1),
     'blender': (3, 3, 0),
     'location': 'Multiple locations',
     'warning': '',
