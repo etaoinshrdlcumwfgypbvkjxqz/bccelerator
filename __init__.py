694a64602fe6cad2bdd2b0dafce60fe8c280345b
William So <polyipseity@gmail.com>
2023-02-18 13:32:31 +0800

    v1.3.1
    Signed-off-by: William So <polyipseity@gmail.com>

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ bl_info = {
     "name": "bccelerator",
     "description": "Contains tools to accelerate Blender workflow.",
     "author": "William So",
-    "version": (1, 3, 0),
+    "version": (1, 3, 1),
     "blender": (3, 3, 0),
     "location": "Multiple locations",
     "warning": "",
