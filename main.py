552abcd9822250541ecd66e87f14a7a28b7e5334
William So <polyipseity@gmail.com>
2023-06-11 11:24:44 +0800

    Remove unnecessary types in `main.py`
    Signed-off-by: William So <polyipseity@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 # -*- coding: bccelerator-transform-UTF-8 -*-
-import types as _types
-import typing as _typing
-
 from .core import tools as _core_tools
 
-_modules: _typing.Collection[_types.ModuleType] = tuple(_core_tools.items())
+_modules = tuple(_core_tools.items())
 
 
 def register():
